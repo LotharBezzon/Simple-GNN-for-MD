38151ce08fbb6e3cd71bffe2858070af70564462
LotharBezzon <lothar.bezzon@gmail.com>
2024-11-26 23:25:27 +0100

    Update DataProcessing.py

--- DataProcessing.py
+++ DataProcessing.py
@@ -14,9 +14,12 @@ def read_data(file):
                 next(lines)  # Skip ITEM: NUMBER OF ATOMS
                 atom_number = int(next(lines))
                 next(lines)  # Skip ITEM: BOX BOUNDS
-                next(lines)
-                next(lines)
-                next(lines)
+                bound_x = next(lines).split()
+                bound_x1, bound_x2 = float(bound_x[0]), float(bound_x[1])
+                bound_y = next(lines).split()
+                bound_y1, bound_y2 = float(bound_y[0]), float(bound_y[1])
+                bound_z = next(lines).split()
+                bound_z1, bound_z2 = float(bound_z[0]), float(bound_z[1])
                 next(lines)  # Skip ITEM: ATOMS id mol type x y z fx fy fz
                 atoms = []
                 for _ in range(atom_number):
@@ -26,14 +29,20 @@ def read_data(file):
                         'mol': int(atom_data[1]),
                         'type': int(atom_data[2]),
                         'x': float(atom_data[3]),
+                        'x+': float(atom_data[3]) + (bound_x2 - bound_x1),
+                        'x-': float(atom_data[3]) - (bound_x2 - bound_x1),
                         'y': float(atom_data[4]),
+                        'y+': float(atom_data[4]) + (bound_y2 - bound_y1),
+                        'y-': float(atom_data[4]) - (bound_y2 - bound_y1),
                         'z': float(atom_data[5]),
+                        'z+': float(atom_data[5]) + (bound_z2 - bound_z1),
+                        'z-': float(atom_data[5]) - (bound_z2 - bound_z1),
                         'fx': float(atom_data[6]),
                         'fy': float(atom_data[7]),
                         'fz': float(atom_data[8])
                     }
                     atoms.append(atom)
-                data.append({'timestep': timestep, 'atoms': atoms})
+                data.append({'timestep': timestep, 'num_atoms': atom_number, 'atoms': atoms})
         return data
 
 # The most essential model I could think about
@@ -65,8 +74,30 @@ def make_basic_graphs(data, k=5):
     return graphs
 
 # Build graphs for a SchNet architecture
-def make_SchNet_graphs(data):
-    pass
+def make_SchNetlike_graphs(data):
+    graphs = []
+    for frame in data:
+        atoms = frame['atoms']
+        x = torch.tensor([[atom['type'] - 0.5] for atom in atoms], dtype=torch.float)
+        
+        # Create a fully connected graph
+        edge_index = torch.combinations(torch.arange(frame['num_atoms']), r=2).t()
+        
+        # Duplicate edges in the opposite direction to make it undirected
+        edge_index = torch.cat([edge_index, edge_index.flip(0)], dim=1)
+        
+        # Edge features are smooth 'one-hot vectors' to represent distances  
+        dist_one_hot = [0.5, 0.96, 1.69, 1.97, 3, 5, 8]    # 0.96 for O-H, 1.69 for H-H, 1.97 for H bonds
+        positions = torch.tensor([[atom['x'], atom['y'], atom['z']] for atom in atoms], dtype=torch.float)
+        start_nodes, end_nodes = edge_index
+        distances = torch.norm(positions[start_nodes] - positions[end_nodes], dim=1)
+        
+        edge_attr = torch.zeros((edge_index.size(1), len(dist_one_hot)), dtype=torch.float)
+        for i, dist in enumerate(dist_one_hot):
+            edge_attr[:, i] = np.e ** (-(distances - dist)**2 / 0.4)    # 0.4 is arbitrary
+
+        y = torch.tensor([(atom['fx'], atom['fy'], atom['fz']) for atom in atoms], dtype=torch.float)
+        graphs.append(Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y))
 
 # Build graphs with encoded node features
 def make_encoded_graphs(data):
