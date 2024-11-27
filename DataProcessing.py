af2a4b39962ec3d400d8696c0edf2aba862f4695
LotharBezzon <lothar.bezzon@gmail.com>
2024-11-27 12:24:01 +0100

    Considering periodic boundaries

--- DataProcessing.py
+++ DataProcessing.py
@@ -15,11 +15,12 @@ def read_data(file):
                 atom_number = int(next(lines))
                 next(lines)  # Skip ITEM: BOX BOUNDS
                 bound_x = next(lines).split()
-                bound_x1, bound_x2 = float(bound_x[0]), float(bound_x[1])
+                size_x = - float(bound_x[0]) + float(bound_x[1])
                 bound_y = next(lines).split()
-                bound_y1, bound_y2 = float(bound_y[0]), float(bound_y[1])
+                size_y = - float(bound_y[0]) + float(bound_y[1])
                 bound_z = next(lines).split()
-                bound_z1, bound_z2 = float(bound_z[0]), float(bound_z[1])
+                size_z = - float(bound_z[0]) + float(bound_z[1])
+                box_size = torch.tensor([size_x, size_y, size_z], dtype=torch.float)
                 next(lines)  # Skip ITEM: ATOMS id mol type x y z fx fy fz
                 atoms = []
                 for _ in range(atom_number):
@@ -29,21 +30,23 @@ def read_data(file):
                         'mol': int(atom_data[1]),
                         'type': int(atom_data[2]),
                         'x': float(atom_data[3]),
-                        'x+': float(atom_data[3]) + (bound_x2 - bound_x1),
-                        'x-': float(atom_data[3]) - (bound_x2 - bound_x1),
                         'y': float(atom_data[4]),
-                        'y+': float(atom_data[4]) + (bound_y2 - bound_y1),
-                        'y-': float(atom_data[4]) - (bound_y2 - bound_y1),
                         'z': float(atom_data[5]),
-                        'z+': float(atom_data[5]) + (bound_z2 - bound_z1),
-                        'z-': float(atom_data[5]) - (bound_z2 - bound_z1),
                         'fx': float(atom_data[6]),
                         'fy': float(atom_data[7]),
                         'fz': float(atom_data[8])
                     }
                     atoms.append(atom)
-                data.append({'timestep': timestep, 'num_atoms': atom_number, 'atoms': atoms})
+                data.append({'timestep': timestep, 'num_atoms': atom_number, 'box_size': box_size, 'atoms': atoms})
         return data
+
+# To account for periodic boundary conditions
+def minimum_image_distance(coords1, coords2, box_size):
+
+  delta = coords1 - coords2
+  delta -= torch.round(delta / box_size) * box_size
+  distance = torch.norm(delta)
+  return distance
 
 # The most essential model I could think about
 def make_basic_graphs(data, k=5):
@@ -90,14 +93,15 @@ def make_SchNetlike_graphs(data):
         dist_one_hot = [0.5, 0.96, 1.69, 1.97, 3, 5, 8]    # 0.96 for O-H, 1.69 for H-H, 1.97 for H bonds
         positions = torch.tensor([[atom['x'], atom['y'], atom['z']] for atom in atoms], dtype=torch.float)
         start_nodes, end_nodes = edge_index
-        distances = torch.norm(positions[start_nodes] - positions[end_nodes], dim=1)
+        distances = minimum_image_distance(positions[start_nodes], positions[end_nodes], frame['box_size'])
         
         edge_attr = torch.zeros((edge_index.size(1), len(dist_one_hot)), dtype=torch.float)
         for i, dist in enumerate(dist_one_hot):
-            edge_attr[:, i] = np.e ** (-(distances - dist)**2 / 0.4)    # 0.4 is arbitrary
+            edge_attr[:, i] = np.e ** (-(distances - dist)**2 / 0.4)    # 0.4 is quite arbitrary
 
         y = torch.tensor([(atom['fx'], atom['fy'], atom['fz']) for atom in atoms], dtype=torch.float)
         graphs.append(Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y))
+    return graphs
 
 # Build graphs with encoded node features
 def make_encoded_graphs(data):
